2ccf49c5fcac19b33284e7c31a424ddbb08ea7ab
Emily Mendoza <112563765+EmilyMV@users.noreply.github.com>
2023-10-13 00:06:17 -0600

    Creación de práctica 1 con Ant

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Mendoza Valdez Emily Isabela
+ */
+import java.util.Scanner;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n1;
+        System.out.println("Hola Mundo desde POO gpo 8");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa un número: ");
+        n1 = sc.nextInt();
+        System.out.println("El número es:"+n1);
+    }
+    
+}
